d47c9411613f85514dd59bd29b939e49474be415
Mantounio <mahdidev83@gmail.com>
2024-05-15 23:58:06 +0330

    Sum arrayList Question

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Person
+{
+
+    public static int simpleArraySum(List<int> ar)
+    {
+        int sum = 0;
+        for (int i = 0; i < ar.Count(); i++)
+        {
+            sum += ar[i];
+        }
+        return sum;
+    }
+
+    static void Main(String[] args)
+    {
+        Console.WriteLine(simpleArraySum(new List<int>() { 1, 2, 3 }));
+        Console.ReadLine();
+    }
+}
