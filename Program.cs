f478b042a7b921ecfd2dee2e43d56711227b57ab
mahdidev-83 <72861788+mahdidev-83@users.noreply.github.com>
2024-05-17 14:30:59 +0330

    added the sayMoreThanOne method to print more than one time

--- Program.cs
+++ Program.cs
@@ -15,11 +15,23 @@ class Person
         return sum;
     }
 
+    public static void sayMoreThanOneTime(int iterationNumber)
+    {
+        for(int i = 0;i<= iterationNumber;i++)
+        {
+            Console.WriteLine($"the number of iteration of this Program is {i}:");
+        }
+    }
+
     static void Main(String[] args)
     {
         Console.WriteLine(simpleArraySum(new List<int>() { 1, 2, 3 }));
         Console.ReadLine();
 
         // the changes from this code
+
+
+        // the sayThanOneTime method:
+        sayMoreThanOneTime(4);
     }
 }
